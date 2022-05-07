b640c56d5f59d670d9d6142184c36b407eac003b
TungSC <tungngo@gempages.help>
2022-05-07 10:40:18 +0700

    four commit

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "os"
-    "os/exec"
-    "strconv"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
 
-    "control-server/db"
-    "github.com/joho/godotenv"
+	"control-server/db"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 var Redis *db.Redis
@@ -16,39 +18,51 @@ var Redis *db.Redis
 const RedisPubSubChannel = "redis_pub_sub_server_resource"
 
 func main() {
-    godotenv.Load()
+	godotenv.Load()
 
-    dbConfig := os.Getenv("REDIS_DB")
-    serverEndpoint := os.Getenv("SERVER_ENDPOINT")
-    serverPort := os.Getenv("SERVER_PORT")
-    dbNum, _ := strconv.Atoi(dbConfig)
+	r := gin.Default()
 
-    Redis = db.NewRedis(db.RedisConfig{
-        Addrs: []string{os.Getenv("REDIS_SERVER")},
-        Pwd:   os.Getenv("REDIS_PASSWORD"),
-        DB:    dbNum,
-    })
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Accept", "Accept-Language", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
-    topic := Redis.Subscribe(RedisPubSubChannel)
-    channel := topic.Channel()
-    port := fmt.Sprintf("%s:%s", serverEndpoint, serverPort)
+	dbConfig := os.Getenv("REDIS_DB")
+	serverEndpoint := os.Getenv("SERVER_ENDPOINT")
+	serverPort := os.Getenv("SERVER_PORT")
+	dbNum, _ := strconv.Atoi(dbConfig)
 
-    for msg := range channel {
-        var receiveMessage db.ReceiveMessage
-        // Unmarshal the data into the user
-        err := json.Unmarshal([]byte(msg.Payload), &receiveMessage)
-        if err != nil {
-            _ = fmt.Errorf("json.Unmarshal: %w", err)
-        }
+	Redis = db.NewRedis(db.RedisConfig{
+		Addrs: []string{os.Getenv("REDIS_SERVER")},
+		Pwd:   os.Getenv("REDIS_PASSWORD"),
+		DB:    dbNum,
+	})
 
-        if receiveMessage.Server == port {
-            if receiveMessage.Action == "active" {
-                exec.Command("sh", "-c", "pm2 start main").Run()
-            }
+	topic := Redis.Subscribe(RedisPubSubChannel)
+	channel := topic.Channel()
+	port := fmt.Sprintf("%s:%s", serverEndpoint, serverPort)
 
-            if receiveMessage.Action == "inactive" {
-                exec.Command("sh", "-c", "pm2 stop main").Run()
-            }
-        }
-    }
+	for msg := range channel {
+		var receiveMessage db.ReceiveMessage
+		// Unmarshal the data into the user
+		err := json.Unmarshal([]byte(msg.Payload), &receiveMessage)
+		if err != nil {
+			_ = fmt.Errorf("json.Unmarshal: %w", err)
+		}
+
+		if receiveMessage.Server == port {
+			if receiveMessage.Action == "active" {
+				exec.Command("sh", "-c", "pm2 start main").Run()
+			}
+
+			if receiveMessage.Action == "inactive" {
+				exec.Command("sh", "-c", "pm2 stop main").Run()
+			}
+		}
+	}
 }
